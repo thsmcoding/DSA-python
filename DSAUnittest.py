3c494f25eeec806d2ae70122023ddc9477393257
thsmcoding <hassia.tamimou@gmail.com>
2022-06-17 13:55:36 +0200

    FIX: fixing test cases for exceptions

--- DSAUnittest.py
+++ DSAUnittest.py
@@ -3,15 +3,34 @@ import unittest.mock
 from primermodule import finding_number
 from primermodule import rd
 
-class DSAUnittest(ut.TestCase):
-    def test_check_finding_missing_number(self):
+class DSATestMethods(ut.TestCase):
+    def test_finding_number_success(self):
+        print("###### Unit Test : finding_number SUCCESS ####### \n")
         self.input=12
         l=list(range(self.input))
         removed=rd.randrange(self.input)
         l.remove(removed)
         print("TEST : we have removed ",removed)
-        result=finding_number(self.input,l)
-        self.assertEqual(removed,result)
+        actual=finding_number(self.input,l)
+        expected= removed
+        self.assertEqual(actual, expected)
 
+    def test_finding_number_valueexception(self):
+        print("###### Unit Test : ValueError ####### \n")
+        self.size=1
+        with self.assertRaises(ValueError) as exception_context:
+            finding_number(n=self.size,s=list((range(self.size))))
+        self.assertEqual(str(exception_context.exception),
+                         "Detected problem with your input. Check")
+        
+    def test_finding_number_indexexception(self):
+        print("###### Unit Test : Index Error ####### \n")
+        self.inputlist=[]
+        self.size=2
+        with self.assertRaises(IndexError) as exception_context:
+            finding_number(self.size,self.inputlist)
+        self.assertEqual(str(exception_context.exception),
+                         "The list is empty or None.")
+    
 if __name__== '__main__':
     ut.main()
--- primermodule.py
+++ primermodule.py
@@ -1,15 +1,14 @@
 import random as rd
 
 def finding_number(n,s):
-    try:
-        if n<=0:
+        if n <= 1:
             raise ValueError("Detected problem with your input. Check")
-        if len(s)==0 or s is None:
-            raise IndexError("The list is empty or not declared.")
-    finally:
-        print("You have chosen number:", n)
-        print("This is the initialized sequence ",s)
-        missing=sum(range(n)) - sum(s)
-        print("The missing number is", missing)
-        return missing
-
+        elif len(s) < 2 or s is None:
+            raise IndexError("The list is empty or None.")
+        else:
+            print("You have chosen number:", n)
+            print("This is the initialized sequence ",s)
+            missing=sum(range(n)) - sum(s)
+            print("The missing number is", missing)
+            return missing
+    
