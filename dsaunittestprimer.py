38739b870843a46a1515bc58f390e6db6ec694a5
thsmcoding <hassia.tamimou@gmail.com>
2022-09-12 18:09:02 +0200

    API: adding permutation search

--- dsaunittestprimer.py
+++ dsaunittestprimer.py
@@ -1,7 +1,8 @@
 import unittest as ut
 import unittest.mock
-from primermodule import finding_number
-from primermodule import rd
+from math import factorial
+from primermodule import rd, finding_number, permutations
+
 
 class DSATestMethods(ut.TestCase):
     def test_finding_number_success(self):
@@ -31,6 +32,25 @@ class DSATestMethods(ut.TestCase):
             finding_number(self.size,self.inputlist)
         self.assertEqual(str(exception_context.exception),
                          "The list is empty or None.")
-    
+        
+    def test_permuations(self):
+        print("TESTING PERMUTATIONS FOR 0 CHARACTERS:")
+        self.l= []
+        self.n= len(self.l)
+        self.count=[0]
+        permutations(self.l,0,self.n, self.count)
+        actual, expected =0,self.count[0]
+        self.assertEqual(actual, expected)
+
+    def test_permuations(self):
+        print("TESTING PERMUTATIONS FOR MULTIPLE CHARACTERS: \n")
+        self.l= ['C','A','T', 'D', 'O', 'G']
+        self.n= len(self.l)
+        self.count=[0]
+        permutations(self.l,0,self.n-1, self.count)
+        print("Search has found "+ str(self.count[0])+ " permutations")
+        actual, expected= self.count[0], factorial(self.n)
+        self.assertEqual(actual, expected)
+        
 if __name__== '__main__':
     ut.main()
